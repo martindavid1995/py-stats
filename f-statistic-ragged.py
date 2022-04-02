504ca816a5562a4ae96df9187eb3ba5486349166
martindavid1995 <48227232+martindavid1995@users.noreply.github.com>
2022-04-02 11:37:40 -0400

    create f-statistic-ragged

---
+++ f-statistic-ragged.py
@@ -0,0 +1,99 @@
+import numpy as np
+import termtables as tt
+import scipy.stats as sps  
+import warnings
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
+
+def summation(data: np.array):
+    sum = 0
+    for i in range(len(data)):
+        arr = data[i]
+        for j in range(len(arr)):
+            sum += data[i][j] 
+    return sum
+
+def sum_x_js(data: np.array):
+    tot_sum = 0
+    for i in range(len(data)):
+        part_sum = sum(data[i])
+        tot_sum += (part_sum**2)/len(data[i])
+    return tot_sum    
+        
+
+def size(data: np.array):
+    size = 0
+    for i in range(len(data)):
+        arr = data[i]
+        for j in range(len(arr)):
+            size += 1 
+    return size  
+
+def sum_of_squares(data: np.array):
+    sum = 0
+    for i in range(len(data)):
+        arr = data[i]
+        for j in range(len(arr)):
+            sum += data[i][j]**2  
+    return sum
+
+def SST(data: np.array):
+    return sum_of_squares(data) - ((1/size(data))*(summation(data)**2))
+
+def SSTr(data: np.array):
+    return sum_x_js(data) - ((1/size(data))*(summation(data)**2))
+
+def SSE(data: np.array):
+    return SST(data)-SSTr(data)
+
+def MSTr(data: np.array):
+    return SSTr(data)/(len(data)-1)
+
+def MSE(data: np.array):
+    return SSE(data)/(size(data)-len(data))
+
+def f(data: np.array):
+    return MSTr(data)/MSE(data)
+
+def get_table(data):
+    I = len(data)
+    J = len(data[0])
+    v1 = I - 1
+    v2 = I*(J-1)
+    _SSTR = round(SSTr(data),2)
+    _SSE = round(SSE(data),2)
+    _SST = round(SST(data),2)
+    _MSTR = round(MSTr(data),2)
+    _MSE = round(MSE(data),2)
+    _f = round(f(data),2)
+    
+    
+    table = tt.to_string(
+    [["Treatements", v1, _SSTR, _MSTR, _f], ["Error", v2, _SSE, _MSE, "X"], ["Total", v1+v2, _SST, "X", "X"]],
+    header=["Source of Variation", "Degrees of Freedom", "Sum of Squares", "Mean Square", "f"],
+    style=tt.styles.ascii_thin_double)
+    return(table) 
+
+def hypTest(data, alpha):
+    I = len(data)
+    J = len(data[0])
+    v1 = I - 1
+    v2 = I*(J-1)
+    _f = round(f(data),2)
+    _fcrit = sps.f.ppf(q=1-alpha, dfn=v1, dfd=v2).round(2);
+    if _f > _fcrit:
+        print(_f, " > ", _fcrit, "which means p < a so we reject H0")
+    else:
+        print(_f, " < ", _fcrit, "which means p > a so we fail to reject H0")
+
+def main():
+    data = np.array([[67,50,70,60,55,75],
+                     [49,32,65,39,43],
+                     [40,39,41,60,45,30,28]])
+    alpha = .05
+    
+    print(get_table(data))
+    hypTest(data, alpha)
+
+if __name__ == "__main__":
+    main()
+    
